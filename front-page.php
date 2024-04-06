900ff58924aed5dd6840655c82cda743d9e5b5c0
hugues leger <hugueslegerdevweb@gmail.com>
2024-04-06 22:53:11 +0200

    add custom post type card + list services wp

--- front-page.php
+++ front-page.php
@@ -1,30 +1,37 @@
 <?php
 
 
-$context          				= Timber::context();
-$context['posts'] 				= Timber::get_posts();
-$timber_post_hero           	= Timber::get_post();
-$context['post_hero']       	= $timber_post_hero;
-$timber_post_insta           	= Timber::get_post();
-$context['post_insta']       	= $timber_post_insta;
-$timber_post_about		   		= Timber::get_post();
-$context['post_about'] 		 	= $timber_post_about;
-$timber_post_video		   		= Timber::get_post();
-$context['post_video'] 		 	= $timber_post_video;
+$context          						= Timber::context();
+$context['posts'] 						= Timber::get_posts();
+$timber_post_hero           			= Timber::get_post();
+$context['post_hero']       			= $timber_post_hero;
+$timber_post_insta           			= Timber::get_post();
+$context['post_insta']       			= $timber_post_insta;
+$timber_post_about		   				= Timber::get_post();
+$context['post_about'] 		 			= $timber_post_about;
+$timber_post_video		   				= Timber::get_post();
+$context['post_video'] 		 			= $timber_post_video;
+$timber_post_service					= Timber::get_post();
+$context['post_service'] 		 		= $timber_post_service;
 
-// $postCardHero = [
-// 	'post_type' => 'card-hero',
-// 	'posts_per_page' => 4,
-// 	'order'		=> 'ASC'
-// ];
 
 $context['posts_card_hero'] = Timber::get_posts([
 	'post_type' => 'card-hero',
 	'posts_per_page' => 4,
 	'order'		=> 'ASC'
 ]);
-//$context['posts_card_hero'] = Timber::get_posts($postCardHero);
-// $timber_post_card_hero     = Timber::get_post('card-hero');
-// $context['post_card_hero'] = $timber_post_card_hero;
+
+$context['posts_list_service'] = Timber::get_posts([
+	'post_type' => 'list-services',
+	'posts_per_page' => 6,
+	'order'		=> 'ASC'
+]);
+
+$context['posts_card_service'] = Timber::get_posts([
+	'post_type' => 'card-services',
+	'posts_per_page' => 3,
+	'order'		=> 'ASC'
+]);
+
 
 Timber::render( 'front-page.twig', $context);
--- home_post/custom-post-home.php
+++ home_post/custom-post-home.php
@@ -42,3 +42,84 @@ function card_hero() {
 }
 card_hero();
 
+/** list services */
+function list_services() {
+    $labels = array(
+        'name'               => 'Liste services',
+        'singular_name'      => 'list service',
+        'menu_name'          => 'Liste services',
+        'name_admin_bar'     => 'Liste services',
+        'add_new'            => 'Ajouter un nouveau service',
+        'add_new_item'       => 'Ajouter un service',
+        'new_item'           => 'Nouveau service',
+        'edit_item'          => 'Editer un service',
+        'view_item'          => 'Voir un service',
+        'all_items'          => 'Liste services',
+        'search_items'       => 'Rechercher un service',
+        'parent_item_colon'  => 'Parent Item',
+        'not_found'          => 'Aucun service trouvé',
+        'not_found_in_trash' => 'Aucun service dans la corbeille'
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'description'        => __( 'Description.', 'your-plugin-textdomain' ),
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => 'home_post/custom-post-home.php',
+        'show_in_rest'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'list-services' ),
+        'capability_type'    => 'post',
+        'menu_icon'          => 'dashicons-smiley',
+        'has_archive'        => false,
+        'hierarchical'       => true,
+        'menu_position'      => 4,
+        'supports'           => array( 'title', 'editor', 'author', 'excerpt', 'thumbnail','revisions','page-attributes'),
+    );
+
+    register_post_type( 'list-services', $args );
+}
+list_services();
+
+/** list services */
+function card_services() {
+    $labels = array(
+        'name'               => 'Card services',
+        'singular_name'      => 'card service',
+        'menu_name'          => 'Card services',
+        'name_admin_bar'     => 'Card services',
+        'add_new'            => 'Ajouter un nouvelle card',
+        'add_new_item'       => 'Ajouter une card',
+        'new_item'           => 'Nouvelle card',
+        'edit_item'          => 'Editer une card',
+        'view_item'          => 'Voir une card',
+        'all_items'          => 'Card services',
+        'search_items'       => 'Rechercher une card',
+        'parent_item_colon'  => 'Parent Item',
+        'not_found'          => 'Aucune card trouvée',
+        'not_found_in_trash' => 'Aucune card dans la corbeille'
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'description'        => __( 'Description.', 'your-plugin-textdomain' ),
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => 'home_post/custom-post-home.php',
+        'show_in_rest'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'card-services' ),
+        'capability_type'    => 'post',
+        'menu_icon'          => 'dashicons-smiley',
+        'has_archive'        => false,
+        'hierarchical'       => true,
+        'menu_position'      => 4,
+        'supports'           => array( 'title', 'editor', 'thumbnail'),
+    );
+
+    register_post_type( 'card-services', $args );
+}
+card_services();
